be1cfa20fc9f4ef0b54b106bf9ef06c462f73b3b
shiva2007-web <gnanashiva07@gmail.com>
2026-04-21 00:36:07 +0530

    Create branch and implement UC3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,56 +1,20 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
-
-    static char[][] board = new char[3][3];
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
+    
     public static void main(String[] args) {
-        initializeBoard();
-        tossAndAssignSymbols();
-        displayTossResult();
-        printBoard();
+        // Testing UC3 logic
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
 
-    static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
-    }
-
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println("\n-------------");
-        }
-    }
-
-    static void tossAndAssignSymbols() {
-        Random rand = new Random();
-        isHumanTurn = rand.nextBoolean();
-        if (isHumanTurn) {
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("Toss Result: Human plays first.");
-        } else {
-            System.out.println("Toss Result: Computer plays first.");
-        }
-        System.out.println("Human: " + humanSymbol + " | Computer: " + computerSymbol);
+    /**
+     * UC3: Reads an integer slot value (1-9) from the user.
+     */
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = sc.nextInt();
+        return slot;
     }
 }
